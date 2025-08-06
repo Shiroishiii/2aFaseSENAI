9a924e9426ff11487f537bc53ca20409fc300135
Shiroishiii <isaacdantas2503@gmail.com>
2025-08-05 22:09:31 -0300

    Primeira aula intro de React

---
+++ projeto/introreact/src/components/MotivosPraUsarReact.jsx
@@ -0,0 +1,9 @@
+import './MotivosPraUsarReact.css'
+
+function MotivosPraUsarReact() {
+  return (
+    <p className="motivo">1. Um componente criado pode ser reutilizado</p>
+  )
+}
+
+export default MotivosPraUsarReact
