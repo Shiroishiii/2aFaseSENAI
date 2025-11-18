dbc62f34b2d0dfc00089eacf3dc7d2e58c740148
Shiroishiii <isaacdantas2503@gmail.com>
2025-11-17 22:04:14 -0300

    aula banco de dados

---
+++ aula-banco de dados/src/app.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import conexao from '../infra/conexao.js'
+
+const app = express()
+
+app.use(express.json())
+
+//criar rota
+
+app.get('/estudantes', (req, res) =>{
+    const sql = 'SELECT * FROM dados_estudantes'
+    conexao.query(sql,(erro, resultado) =>{
+        if(erro){
+            console.log(erro)
+        } else {
+            res.json(resultado)
+        }
+    })
+})
+
+app.get('/estudantes/:id', (req, res) =>{
+    const id = req.params.id
+    const sql = 'SELECT * FROM dados_estudantes WHERE id_estudante=?'
+    conexao.query(sql, id, (erro, resultado) =>{
+        if(erro){
+            console.log(erro)
+        } else {
+            res.json(resultado)
+        }
+    })
+})
+
+app.post('/estudantes', (req, res) =>{
+    const estudante = req.body
+    const sql = 'INSERT INTO dados_estudantes SET ?'
+    conexao.query(sql, estudante, (erro, resultado) =>{
+        if(erro){
+            console.log(erro)
+        } else {
+            res.json(resultado)
+        }
+    })
+})
+
+export default app
